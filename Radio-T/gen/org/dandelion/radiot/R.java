5a3a3dd9cd80179e72918de8941be62f4155cebd
Sergey Moshnikov <s.moshnikov@gmail.com>
2010-06-30 09:46:15 +0400

    Added logo image to the home screen

--- Radio-T/gen/org/dandelion/radiot/R.java
+++ Radio-T/gen/org/dandelion/radiot/R.java
@@ -16,6 +16,7 @@ public final class R {
     public static final class drawable {
         public static final int background=0x7f020000;
         public static final int icon=0x7f020001;
+        public static final int logo=0x7f020002;
     }
     public static final class id {
         public static final int logo_text=0x7f070000;
