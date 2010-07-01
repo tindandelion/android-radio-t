04de7129799fec1bf825090258efe979cc83323d
Sergey Moshnikov <s.moshnikov@gmail.com>
2010-07-01 08:31:08 +0400

    Added a sample rss for pirates

--- Radio-T/src/org/dandelion/radiot/HomeScreen.java
+++ Radio-T/src/org/dandelion/radiot/HomeScreen.java
@@ -4,7 +4,6 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
