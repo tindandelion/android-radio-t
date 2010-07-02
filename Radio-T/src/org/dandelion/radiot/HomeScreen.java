f3bb4c73e03ce2fa0922f20552ab361fd2f0bbc1
Sergey Moshnikov <s.moshnikov@gmail.com>
2010-07-02 08:03:36 +0400

    Make acceptance tests pass

--- Radio-T/src/org/dandelion/radiot/HomeScreen.java
+++ Radio-T/src/org/dandelion/radiot/HomeScreen.java
@@ -1,5 +1,10 @@
 package org.dandelion.radiot;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.dandelion.radiot.HomeScreen.HomeScreenItem;
+
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +15,7 @@ import android.widget.ListView;
 public class HomeScreen extends ListActivity {
 	private static final String PODCAST_URL = "http://feeds.rucast.net/radio-t";
 	private static final String PIRATES_URL = "http://feeds.feedburner.com/pirate-radio-t";
+	private ArrayAdapter<HomeScreenItem> listAdapter;
 	
 
 	@Override
@@ -21,15 +27,52 @@ public class HomeScreen extends ListActivity {
 	
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
+		HomeScreenItem item = listAdapter.getItem(position);
+		item.execute();
+	}
+
+	private void startPodcastListActivityForUrl(String url) {
 		Intent intent = new Intent(this, PodcastListActivity.class);
-		intent.putExtra(PodcastListActivity.URL_KEY, PIRATES_URL);
+		intent.putExtra(PodcastListActivity.URL_KEY, url);
 		startActivity(intent);
 	}
 
 	private void initList() {
-		String[] menu_items = getResources().getStringArray(R.array.main_menu);
-		ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this,
-				R.layout.home_screen_item, R.id.home_screen_item_title, menu_items);
+		listAdapter = new ArrayAdapter<HomeScreenItem>(this,
+				R.layout.home_screen_item, R.id.home_screen_item_title, getHomeSceenItems());
 		setListAdapter(listAdapter);
+	}
+
+	private List<HomeScreenItem> getHomeSceenItems() {
+		List<HomeScreenItem> items = new ArrayList<HomeScreenItem>();
+		items.add(new HomeScreenItem("Подкасты") {
+			@Override
+			public void execute() {
+				startPodcastListActivityForUrl(PODCAST_URL);
+			}
+		});
+		items.add(new HomeScreenItem("После-шоу") {
+			@Override
+			public void execute() {
+				startPodcastListActivityForUrl(PIRATES_URL);
+			}
+		});
+		return items;
+	}
+	
+	class HomeScreenItem {
+		private String title;
+
+		public HomeScreenItem(String title) {
+			this.title = title;
+		}
+		
+		public void execute() {
+		}
+
+		@Override
+		public String toString() {
+			return title;
+		}
 	}
 }
