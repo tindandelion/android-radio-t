1f035b60df11fdee102d664212f8f6e73e687913
Sergey Moshnikov <s.moshnikov@gmail.com>
2010-07-12 01:34:50 +0400

    Added an icon to home screen items

--- Radio-T/gen/org/dandelion/radiot/R.java
+++ Radio-T/gen/org/dandelion/radiot/R.java
@@ -14,6 +14,7 @@ public final class R {
         public static final int background=0x7f020000;
         public static final int icon=0x7f020001;
         public static final int logo=0x7f020002;
+        public static final int podcast_icon=0x7f020003;
     }
     public static final class id {
         public static final int home_screen_item_title=0x7f060003;
