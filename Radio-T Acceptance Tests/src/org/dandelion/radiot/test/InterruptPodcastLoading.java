f5e6cf01c8f5380a399452ba5a62168cea755368
Sergey Moshnikov <s.moshnikov@gmail.com>
2010-07-19 10:21:23 +0400

    Moved locked factory func into the test case

--- Radio-T Acceptance Tests/src/org/dandelion/radiot/test/InterruptPodcastLoading.java
+++ Radio-T Acceptance Tests/src/org/dandelion/radiot/test/InterruptPodcastLoading.java
@@ -4,35 +4,56 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
-import org.dandelion.radiot.PodcastListPresenter;
 import org.dandelion.radiot.PodcastItem;
 import org.dandelion.radiot.PodcastList;
 import org.dandelion.radiot.PodcastList.Factory;
 import org.dandelion.radiot.PodcastList.IModel;
 import org.dandelion.radiot.PodcastList.IPresenter;
 import org.dandelion.radiot.PodcastListActivity;
+import org.dandelion.radiot.PodcastListPresenter;
 import org.dandelion.radiot.test.helpers.ApplicationDriver;
 import org.dandelion.radiot.test.helpers.BasicAcceptanceTestCase;
 
-import android.app.Instrumentation;
 import android.content.pm.ActivityInfo;
 
-public class InterruptPodcastLoading extends
-		BasicAcceptanceTestCase {
+public class InterruptPodcastLoading extends BasicAcceptanceTestCase {
 
 	private ApplicationDriver appDriver;
-	private LockedPodcastListFactory factory;
-	
+	private CountDownLatch podcastRetrievalLatch;
+
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
 		appDriver = createApplicationDriver();
 	}
-	
+
 	@Override
 	protected Factory createPodcastListFactory() {
-		factory = new LockedPodcastListFactory(getInstrumentation());
-		return factory;
+		return new LocalRssFeedFactory(getInstrumentation()) {
+			public IModel createModel(String url) {
+				return createTestModel();
+			};
+
+			public IPresenter createPresenter(IModel model) {
+				return createTestPresenter(model);
+			};
+		};
+	}
+
+	protected IPresenter createTestPresenter(IModel model) {
+		return new PodcastListPresenter.AsyncPresenter(model);
+	}
+
+	protected IModel createTestModel() {
+		podcastRetrievalLatch = new CountDownLatch(1);
+
+		return new PodcastList.IModel() {
+			@Override
+			public List<PodcastItem> retrievePodcasts() throws Exception {
+				podcastRetrievalLatch.await();
+				return new ArrayList<PodcastItem>();
+			}
+		};
 	}
 
 	public void testCancelRssLoadingWhenPressingBack() throws Exception {
@@ -41,59 +62,33 @@ public class InterruptPodcastLoading extends
 		try {
 			appDriver.assertOnHomeScreen();
 		} finally {
-			factory.allowPodcastRetrievalFinish();
+			allowPodcastRetrievalToFinish();
 		}
 	}
-	
+
+	protected void allowPodcastRetrievalToFinish() {
+		podcastRetrievalLatch.countDown();
+	}
+
 	public void testDestroyingActivityWhileLoading() throws Exception {
 		PodcastListActivity activity = appDriver.visitMainShowPage();
 		activity.finish();
 		try {
-			factory.allowPodcastRetrievalFinish();
+			allowPodcastRetrievalToFinish();
 			appDriver.assertOnHomeScreen();
 		} catch (Exception e) {
 			fail("Should not have failed");
 		}
 	}
-	
+
 	public void testChangeOrientation() throws Exception {
 		PodcastListActivity activity = appDriver.visitMainShowPage();
 		activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 		try {
-			factory.allowPodcastRetrievalFinish();
+			allowPodcastRetrievalToFinish();
 			appDriver.waitSomeTime();
-		} catch(Exception ex) { 
+		} catch (Exception ex) {
 			fail("The orientation change failed: " + ex.getMessage());
 		}
-	}
-}
-
-class LockedPodcastListFactory extends LocalRssFeedFactory {
-
-	private CountDownLatch modelLatch;
-
-	public LockedPodcastListFactory(Instrumentation instrumentation) {
-		super(instrumentation);
-		modelLatch = new CountDownLatch(1);
-	}
-	
-	@Override
-	public IPresenter createPresenter(IModel model) {
-		return new PodcastListPresenter.AsyncPresenter(model);
-	}
-	
-	@Override
-	public IModel createModel(String url) {
-		return new PodcastList.IModel() {
-			@Override
-			public List<PodcastItem> retrievePodcasts() throws Exception {
-				modelLatch.await();
-				return new ArrayList<PodcastItem>();
-			}
-		};
-	}
-	
-	public void allowPodcastRetrievalFinish() {
-		modelLatch.countDown();
 	}
 }
