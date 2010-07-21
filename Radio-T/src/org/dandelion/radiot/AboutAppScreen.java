66746e5dc352b82eed8a3cf22fc2e82515e08e60
Sergey Moshnikov <s.moshnikov@gmail.com>
2010-07-21 13:35:59 +0400

    Feedback click handler stub, some tweking in style

--- Radio-T/src/org/dandelion/radiot/AboutAppScreen.java
+++ Radio-T/src/org/dandelion/radiot/AboutAppScreen.java
@@ -2,11 +2,17 @@ package org.dandelion.radiot;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class AboutAppScreen extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.about_app);
+	}
+
+	public void sendFeedback(View view) {
+		Toast.makeText(this, "Sending feedback", Toast.LENGTH_SHORT).show();
 	}
 }
