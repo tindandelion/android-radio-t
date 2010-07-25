462aa82b62f7ff95bfcd8a769f6acc321ea4493c
Sergey Moshnikov <s.moshnikov@gmail.com>
2010-07-25 15:03:14 +0400

    PresenterTestCase sterilized

--- Radio-T-Test/src/org/dandelion/radiot/unittest/PodcastListPresenterTestCase.java
+++ Radio-T-Test/src/org/dandelion/radiot/unittest/PodcastListPresenterTestCase.java
@@ -2,9 +2,9 @@ package org.dandelion.radiot.unittest;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.LinkedBlockingQueue;
 
+import junit.framework.Assert;
 import junit.framework.TestCase;
 
 import org.dandelion.radiot.PodcastItem;
@@ -17,92 +17,57 @@ import android.graphics.Bitmap;
 import android.os.Looper;
 
 public class PodcastListPresenterTestCase extends TestCase {
-	private IPresenter presenter;
-	protected List<PodcastItem> publishedPodcasts;
-	private CountDownLatch podcastListPublishedLatch;
-	private CountDownLatch updateFinishedLatch;
-	private int updatedPodcastImage;
+
 	private TestModel model;
-	
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-		podcastListPublishedLatch = new CountDownLatch(1);
-		updateFinishedLatch = new CountDownLatch(1);
-		updatedPodcastImage = -1;
-		model = new TestModel();
-		presenter = newPresenter();
-	}
+	private TestView view;
+	private IPresenter presenter;
 
 	public void testRetrieveAndPublishPodcastList() throws Exception {
 		ArrayList<PodcastItem> podcastList = newPodcastList();
-		
-		startPodcastListUpdate();
-		model.returnsPodcasts(podcastList);
-		waitUntilPodcastListPublished();
-		
-		assertPublishedPodcasts(podcastList);
-	}
-	
-	public void testRetrievePodcastImagesAfterPublishingList() throws Exception {
-		ArrayList<PodcastItem> podcastList = newPodcastList();
-		PodcastItem item = new PodcastItem();
-		podcastList.add(item);
-		
-		model.returnsPodcasts(podcastList);
-		startPodcastListUpdate();
-		waitUntilPodcastListPublished();
-		assertNull(item.getImage());
-		
-		Bitmap image = Bitmap.createBitmap(1, 1, Bitmap.Config.RGB_565);
-		model.returnsPodcastImage(image);
-		waitUntilAllUpdateIsFinsihed();
-		assertEquals(image, item.getImage());
-	}
-	
-	public void testUpdatingViewWhenPodcastImageIsLoaded() throws Exception {
-		ArrayList<PodcastItem> podcastList = newPodcastList();
-		PodcastItem item = new PodcastItem();
-		podcastList.add(item);
-		
-		model.returnsPodcasts(podcastList);
-		startPodcastListUpdate();
-		waitUntilPodcastListPublished();
-		assertNull(item.getImage());
-		
-		Bitmap image = Bitmap.createBitmap(1, 1, Bitmap.Config.RGB_565);
-		model.returnsPodcastImage(image);
-		waitUntilAllUpdateIsFinsihed();
-		assertPodcastImageUpdated(0);
-	}
-	
-	private void assertPodcastImageUpdated(int i) {
-		assertEquals(i, updatedPodcastImage);
-	}
 
-	private void waitUntilAllUpdateIsFinsihed() throws InterruptedException {
-		if (!updateFinishedLatch.await(60, TimeUnit.SECONDS)) {
-			fail("Failed to wait until all update is finished");
-		}
+		startPodcastListUpdate();
+		model.returnsPodcasts(podcastList);
+		
+		view.waitAndCheckUpdatedPodcasts(podcastList);
+	}
+	
+	public void testLoadingPodcastImages() throws Exception {
+		ArrayList<PodcastItem> podcastList = newPodcastList();
+		PodcastItem item = new PodcastItem();
+		podcastList.add(item);
+
+		model.returnsPodcasts(podcastList);
+		startPodcastListUpdate();
+		view.waitUntilPodcastListUpdated();
+		assertNull(item.getImage());
+
+		Bitmap image = Bitmap.createBitmap(1, 1, Bitmap.Config.RGB_565);
+		model.returnsPodcastImage(image);
+		view.assertPodcastImageUpdated(0);
+		assertEquals(image, item.getImage());
 	}
 
 	protected ArrayList<PodcastItem> newPodcastList() {
 		return new ArrayList<PodcastItem>();
 	}
 
-	private void assertPublishedPodcasts(ArrayList<PodcastItem> podcastList) {
-		assertEquals(podcastList, publishedPodcasts);
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		model = new TestModel();
+		view = new TestView();
+		presenter = newPresenter();
 	}
 
-	private void waitUntilPodcastListPublished() throws InterruptedException {
-		if (!podcastListPublishedLatch.await(60, TimeUnit.SECONDS)) { 
-			fail("Failed to wait until podcast list is published");
-		}
+	private IPresenter newPresenter() {
+		PodcastListPresenter p = new PodcastListPresenter(model);
+		p.attach(view);
+		return p;
 	}
 
 	private void startPodcastListUpdate() {
 		new Thread(new Runnable() {
-			
+
 			public void run() {
 				Looper.prepare();
 				presenter.refresh(false);
@@ -110,38 +75,46 @@ public class PodcastListPresenterTestCase extends TestCase {
 			}
 		}).start();
 	}
+}
 
-	private IPresenter newPresenter() {
-		PodcastListPresenter p = new PodcastListPresenter(model) {
-			@Override
-			public void taskFinished() {
-				super.taskFinished();
-				updateFinishedLatch.countDown();
-			}
-		};
-		p.attach(newView());
-		return p;
+class TestView implements IView {
+	private LinkedBlockingQueue<Integer> updatedImages; 
+	private LinkedBlockingQueue<List<PodcastItem>> updatedPodcasts;
+	
+	public TestView() {
+		updatedPodcasts = new LinkedBlockingQueue<List<PodcastItem>>();
+		updatedImages = new LinkedBlockingQueue<Integer>();
+	}
+	
+	public void assertPodcastImageUpdated(int index) throws InterruptedException {
+		Assert.assertEquals(new Integer(index), updatedImages.take());
 	}
 
-	private IView newView() {
-		return new IView() {
-			public void updatePodcasts(List<PodcastItem> podcasts) {
-				publishedPodcasts = podcasts;
-				podcastListPublishedLatch.countDown();
-			}
-			
-			public void showProgress() {
-			}
-			
-			public void showErrorMessage(String errorMessage) {
-			}
-			
-			public void closeProgress() {
-			}
-			
-			public void updatePodcastImage(int index) {
-				updatedPodcastImage = index;
-			}
-		};
+	public void closeProgress() {
+	}
+
+	public void waitAndCheckUpdatedPodcasts(ArrayList<PodcastItem> podcastList) throws InterruptedException {
+		Assert.assertEquals(podcastList, updatedPodcasts.take());
+	}
+
+	public void waitUntilPodcastListUpdated() throws InterruptedException {
+		updatedPodcasts.take();
+	}
+
+	public void showErrorMessage(String errorMessage) {
+	}
+
+	public void showProgress() {
+	}
+
+	public void updatePodcastImage(int index) {
+		try {
+			updatedImages.put(new Integer(index));
+		} catch (InterruptedException e) {
+		}
+	}
+
+	public void updatePodcasts(List<PodcastItem> podcasts) {
+		updatedPodcasts.add(podcasts);
 	}
 }
