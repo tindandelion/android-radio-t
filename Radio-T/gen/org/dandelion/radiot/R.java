adc9bcec2658933621520c50c2f15209b8e9d485
Sergey Moshnikov <s.moshnikov@gmail.com>
2010-08-06 00:16:54 +0400

    Dark blue noisy background

--- Radio-T/gen/org/dandelion/radiot/R.java
+++ Radio-T/gen/org/dandelion/radiot/R.java
@@ -15,15 +15,16 @@ public final class R {
         public static final int after_show_icon=0x7f020001;
         public static final int blue_background=0x7f020002;
         public static final int blue_background_noise=0x7f020003;
-        public static final int default_podcast_image=0x7f020004;
-        public static final int gray_background_noise=0x7f020005;
-        public static final int icon=0x7f020006;
-        public static final int live_show_icon=0x7f020007;
-        public static final int logo=0x7f020008;
-        public static final int podcast_icon=0x7f020009;
-        public static final int podcast_image_background=0x7f02000a;
-        public static final int round_border_box_background=0x7f02000b;
-        public static final int window_background=0x7f02000c;
+        public static final int dark_blue_background_noise=0x7f020004;
+        public static final int default_podcast_image=0x7f020005;
+        public static final int gray_background_noise=0x7f020006;
+        public static final int icon=0x7f020007;
+        public static final int live_show_icon=0x7f020008;
+        public static final int logo=0x7f020009;
+        public static final int podcast_icon=0x7f02000a;
+        public static final int podcast_image_background=0x7f02000b;
+        public static final int round_border_box_background=0x7f02000c;
+        public static final int window_background=0x7f02000d;
     }
     public static final class id {
         public static final int app_name_label=0x7f070001;
