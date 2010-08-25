cf5dc0d23e90315c797a0bc2545c060d53cd0e5f
Sergey Moshnikov <s.moshnikov@gmail.com>
2010-08-25 08:46:45 +0400

    Cleaned up prev impl

--- Radio-T/src/org/dandelion/radiot/live/LiveShowActivity.java
+++ Radio-T/src/org/dandelion/radiot/live/LiveShowActivity.java
@@ -3,72 +3,17 @@ package org.dandelion.radiot.live;
 import org.dandelion.radiot.R;
 
 import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.IBinder;
-import android.view.View;
-import android.widget.Toast;
-import android.widget.ToggleButton;
 
-public class LiveShowActivity extends Activity implements
-		LiveShowService.ILivePlaybackView {
+public class LiveShowActivity extends Activity {
 
 	// public static final String LIVE_SHOW_URL =
 	// "http://stream3.radio-t.com:8181/stream";
 	public static final String LIVE_SHOW_URL = "http://icecast.bigrradio.com/80s90s";
-
-	private ToggleButton playbackButton;
-	protected LiveShowService liveService;
-
-	private ServiceConnection onService = new ServiceConnection() {
-		public void onServiceDisconnected(ComponentName name) {
-			liveService = null;
-		}
-
-		public void onServiceConnected(ComponentName name, IBinder binder) {
-			liveService = ((LiveShowService.LocalBinder) binder).getService();
-			liveService.attach(LiveShowActivity.this);
-			liveService.startPlaying(LIVE_SHOW_URL);
-		}
-	};
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.live_show_screen);
-		playbackButton = (ToggleButton) findViewById(R.id.live_show_playback_button);
-		bindToService();
-	}
-
-	private void bindToService() {
-		Intent intent = new Intent(this, LiveShowService.class);
-		startService(intent);
-		bindService(intent, onService, 0); 
-	}
-	
-	@Override
-	protected void onDestroy() {
-		liveService.detach();
-		unbindService(onService);
-		super.onStop();
-	}
-
-	public void toggleLiveShow(View v) {
-		liveService.togglePlaying(playbackButton.isChecked());
-	}
-
-	public void enableControls(boolean enabled) {
-		playbackButton.setEnabled(enabled);
-	}
-
-	public void setPlaying(boolean playing) {
-		playbackButton.setChecked(playing);
-	}
-
-	public void showPlaybackError() {
-		Toast.makeText(this, getString(R.string.live_show_playback_error),
-				Toast.LENGTH_LONG).show();
 	}
 }
--- Radio-T/src/org/dandelion/radiot/live/LiveShowService.java
+++ Radio-T/src/org/dandelion/radiot/live/LiveShowService.java
@@ -1,164 +1,16 @@
 package org.dandelion.radiot.live;
 
-import org.dandelion.radiot.R;
-import org.dandelion.radiot.RadiotApplication;
-
-import android.app.Notification;
-import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
-import android.media.MediaPlayer;
-import android.media.MediaPlayer.OnErrorListener;
-import android.media.MediaPlayer.OnPreparedListener;
 import android.os.Binder;
 import android.os.IBinder;
 
 public class LiveShowService extends Service {
 	private final IBinder binder = new LocalBinder();
-	private MediaPlayer mediaPlayer;
-
-	private boolean isPreparing = false;
-	private String currentlyPlayingUrl;
-	
-	private IForegrounder fgnd = new IForegrounder() {
-		public void startForeground() {
-			LiveShowService.this.startForeground(1, createNotification());
-		}
-
-		public void stopForeground() {
-			LiveShowService.this.stopForeground(true);
-		}
-		
-		private Notification createNotification() {
-			Notification note = new Notification(R.drawable.status_icon, null,
-					System.currentTimeMillis());
-			PendingIntent i = PendingIntent.getActivity(getApplication(), 0,
-					new Intent(getApplication(), LiveShowActivity.class), 0);
-			note.setLatestEventInfo(getApplication(),
-					getString(R.string.app_name),
-					getString(R.string.live_show_status_string), i);
-			return note;
-		}
-	};
-	
-	private OnErrorListener onError = new OnErrorListener() {
-		public boolean onError(MediaPlayer mp, int what, int extra) {
-			showPlaybackError();
-			isPreparing = false;
-			stopPlaying();
-			return true;
-		}
-	};
-
-	private OnPreparedListener onPrepared = new OnPreparedListener() {
-		public void onPrepared(MediaPlayer mp) {
-			isPreparing = false;
-			mediaPlayer.start();
-			fgnd.startForeground();
-			updateView();
-		}
-	};
-	private ILivePlaybackView playbackView;
-
-	public void attach(ILivePlaybackView view) {
-		playbackView = view;
-		updateView();
-	}
-	
-	public void detach() {
-		playbackView = null;
-	}
 
 	@Override
 	public IBinder onBind(Intent intent) {
 		return binder;
-	}
-
-
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		mediaPlayer = ((RadiotApplication) getApplication()).getMediaPlayer();
-		mediaPlayer.setOnPreparedListener(onPrepared);
-		mediaPlayer.setOnErrorListener(onError);
-	}
-
-	@Override
-	public void onDestroy() {
-		stopPlaying();
-		mediaPlayer.setOnPreparedListener(null);
-		mediaPlayer.setOnErrorListener(null);
-		super.onDestroy();
-	}
-
-	@Override
-	public boolean onUnbind(Intent intent) {
-		if (!mediaPlayer.isPlaying()) { 
-			stopSelf();
-		}
-		return true;
-	}
-
-	public void setForegrounder(IForegrounder value) { 
-		fgnd = value;
-	}
-
-	public void startPlaying(String url) {
-		if (isIdle()) {
-			try {
-				currentlyPlayingUrl = url;
-				mediaPlayer.setDataSource(url);
-				mediaPlayer.prepareAsync();
-				isPreparing = true;
-			} catch (Exception e) {
-				showPlaybackError();
-			}
-			updateView();
-		}
-	}
-
-	public void stopPlaying() {
-		mediaPlayer.reset();
-		fgnd.stopForeground();
-		updateView();
-	}
-	
-	public void togglePlaying(boolean playing) {
-		if (playing) {
-			startPlaying(currentlyPlayingUrl);
-		} else {
-			stopPlaying();
-		}
-	}
-
-	private boolean isIdle() {
-		return !(isPreparing || mediaPlayer.isPlaying());
-	}
-
-	private void showPlaybackError() {
-		if (null != playbackView) {
-			playbackView.showPlaybackError();
-		}
-	}
-
-	private void updateView() {
-		if (null != playbackView) {
-			playbackView.enableControls(!isPreparing);
-			playbackView.setPlaying(mediaPlayer.isPlaying());
-		}
-	}
-
-	public interface IForegrounder { 
-		void startForeground();
-		void stopForeground();
-	}
-
-	public interface ILivePlaybackView {
-		void enableControls(boolean enabled);
-
-		void setPlaying(boolean playing);
-
-		void showPlaybackError();
 	}
 
 	public class LocalBinder extends Binder {
