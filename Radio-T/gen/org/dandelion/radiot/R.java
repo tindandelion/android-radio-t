149bcd8501bd96f818f01980615419c12e1568b3
Sergey Moshnikov <s.moshnikov@gmail.com>
2010-09-05 17:13:15 +0400

    Redesigned about screen

--- Radio-T/gen/org/dandelion/radiot/R.java
+++ Radio-T/gen/org/dandelion/radiot/R.java
@@ -16,43 +16,45 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int about_icon=0x7f020000;
-        public static final int after_show_icon=0x7f020001;
-        public static final int default_podcast_image=0x7f020002;
-        public static final int divider_background=0x7f020003;
-        public static final int icon=0x7f020004;
-        public static final int live_show_caption_icon=0x7f020005;
-        public static final int live_show_icon=0x7f020006;
-        public static final int logo=0x7f020007;
-        public static final int podcast_icon=0x7f020008;
-        public static final int podcast_image_background=0x7f020009;
-        public static final int status_icon=0x7f02000a;
+        public static final int about_app_activity_icon=0x7f020000;
+        public static final int about_icon=0x7f020001;
+        public static final int after_show_icon=0x7f020002;
+        public static final int default_podcast_image=0x7f020003;
+        public static final int divider_background=0x7f020004;
+        public static final int icon=0x7f020005;
+        public static final int live_show_activity_icon=0x7f020006;
+        public static final int live_show_icon=0x7f020007;
+        public static final int logo=0x7f020008;
+        public static final int podcast_icon=0x7f020009;
+        public static final int podcast_image_background=0x7f02000a;
+        public static final int status_icon=0x7f02000b;
     }
     public static final class id {
-        public static final int about_app_title=0x7f080000;
-        public static final int app_name_label=0x7f080002;
-        public static final int feedback_button=0x7f080004;
-        public static final int home_screen_header=0x7f080007;
-        public static final int home_screen_item_icon=0x7f08000a;
-        public static final int home_screen_item_title=0x7f08000b;
-        public static final int live_show_action_button=0x7f080011;
-        public static final int live_show_icon=0x7f08000c;
-        public static final int live_show_info=0x7f08000e;
-        public static final int live_show_title=0x7f08000d;
-        public static final int live_timer_label=0x7f08000f;
-        public static final int logo=0x7f080001;
-        public static final int logo_image=0x7f080006;
-        public static final int logo_text=0x7f080008;
-        public static final int main_title=0x7f080005;
-        public static final int playback_state_label=0x7f080010;
-        public static final int podcast_item_icon=0x7f080012;
-        public static final int podcast_item_view_date=0x7f080014;
-        public static final int podcast_item_view_number=0x7f080013;
-        public static final int podcast_item_view_shownotes=0x7f080015;
-        public static final int podcast_item_view_tags=0x7f080016;
-        public static final int podcasts_menu=0x7f080009;
-        public static final int refresh=0x7f080017;
-        public static final int version_label=0x7f080003;
+        public static final int about_app_icon=0x7f080000;
+        public static final int about_app_title=0x7f080001;
+        public static final int app_name_label=0x7f080003;
+        public static final int feedback_button=0x7f080005;
+        public static final int home_screen_header=0x7f080008;
+        public static final int home_screen_item_icon=0x7f08000b;
+        public static final int home_screen_item_title=0x7f08000c;
+        public static final int live_show_action_button=0x7f080012;
+        public static final int live_show_icon=0x7f08000d;
+        public static final int live_show_info=0x7f08000f;
+        public static final int live_show_title=0x7f08000e;
+        public static final int live_timer_label=0x7f080010;
+        public static final int logo=0x7f080002;
+        public static final int logo_image=0x7f080007;
+        public static final int logo_text=0x7f080009;
+        public static final int main_title=0x7f080006;
+        public static final int playback_state_label=0x7f080011;
+        public static final int podcast_item_icon=0x7f080013;
+        public static final int podcast_item_view_date=0x7f080015;
+        public static final int podcast_item_view_number=0x7f080014;
+        public static final int podcast_item_view_shownotes=0x7f080016;
+        public static final int podcast_item_view_tags=0x7f080017;
+        public static final int podcasts_menu=0x7f08000a;
+        public static final int refresh=0x7f080018;
+        public static final int version_label=0x7f080004;
     }
     public static final class layout {
         public static final int about_app=0x7f030000;
@@ -84,12 +86,13 @@ public final class R {
         public static final int version_label=0x7f04000a;
     }
     public static final class style {
+        public static final int ActivityIcon=0x7f060004;
         public static final int ActivityTitle=0x7f060003;
-        public static final int Divider=0x7f060004;
+        public static final int Divider=0x7f060005;
         public static final int InfoBox=0x7f060000;
         public static final int Radiot_Button=0x7f060002;
         public static final int Radiot_TextView=0x7f060001;
-        public static final int Theme_Radiot=0x7f060005;
-        public static final int Theme_RadiotNoTitle=0x7f060006;
+        public static final int Theme_Radiot=0x7f060006;
+        public static final int Theme_RadiotNoTitle=0x7f060007;
     }
 }
