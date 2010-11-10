f349cc5e05b08a89d1ee75d9093a9d169d0414f3
Sergey Moshnikov <s.moshnikov@gmail.com>
2010-11-10 23:31:01 +0300

    Added a stub for LiveShowTopics control

---
+++ Radio-T/src/org/dandelion/radiot/live/LiveShowTopics.java
@@ -0,0 +1,13 @@
+package org.dandelion.radiot.live;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class LiveShowTopics extends LinearLayout {
+	public LiveShowTopics(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		setBackgroundColor(Color.RED);
+	}
+}
