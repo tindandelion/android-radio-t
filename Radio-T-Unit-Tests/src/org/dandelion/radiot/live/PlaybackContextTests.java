f0eeec96262e44a591a6202e8fa3f74cbb31ed9b
Sergey Moshnikov <sergey.moshnikov@f-secure.com>
2012-02-29 20:35:52 +0400

    Imported Mockito into Radio-T unit test project

--- Radio-T-Unit-Tests/src/org/dandelion/radiot/live/PlaybackContextTests.java
+++ Radio-T-Unit-Tests/src/org/dandelion/radiot/live/PlaybackContextTests.java
@@ -1,14 +1,18 @@
 package org.dandelion.radiot.live;
 
+import android.media.MediaPlayer;
 import org.dandelion.radiot.live.core.PlaybackContext;
+import org.dandelion.radiot.live.core.states.BasicState;
 import org.junit.*;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 
 public class PlaybackContextTests {
-    final PlaybackContext context = new PlaybackContext(null, null);
-
     @Test
     public void creation() {
+        MediaPlayer player = mock(MediaPlayer.class);
+        BasicState.ILiveShowService service = mock(BasicState.ILiveShowService.class);
+        PlaybackContext context = new PlaybackContext(service, player);
         assertNotNull(context);
     }
 }
