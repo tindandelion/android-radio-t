5759d4ba8093edcb057867de790c85de56d9009f
Sergey Moshnikov <s.moshnikov@gmail.com>
2012-03-15 10:24:53 +0400

    Created the fake podcast downloader, but acc test fails because of incorrect activity creation

--- Radio-T/src/org/dandelion/radiot/podcasts/download/PodcastDownloader.java
+++ Radio-T/src/org/dandelion/radiot/podcasts/download/PodcastDownloader.java
@@ -1,20 +1,5 @@
 package org.dandelion.radiot.podcasts.download;
 
-import java.io.File;
-
-public class PodcastDownloader {
-    private PodcastDownloadManager downloadManager;
-    private DownloadFolder destination;
-
-    public PodcastDownloader(PodcastDownloadManager downloadManager, DownloadFolder destination) {
-        this.downloadManager = downloadManager;
-        this.destination = destination;
-    }
-
-    public void downloadPodcast(String url) {
-        destination.ensureExists();
-        File dest = destination.makePathForUrl(url);
-        downloadManager.submitRequest(url, dest);
-    }
-
+public interface PodcastDownloader {
+    void downloadPodcast(String url);
 }
