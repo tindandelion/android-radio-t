f1985edd1fcc837107a2d160d81ef0c7541357ae
Sergey Moshnikov <s.moshnikov@gmail.com>
2012-04-01 16:44:09 +0400

    Notifications from DownloadNotificationManager were extracted into IconNotification class

--- Radio-T/src/org/dandelion/radiot/podcasts/download/DownloadNotificationManager.java
+++ Radio-T/src/org/dandelion/radiot/podcasts/download/DownloadNotificationManager.java
@@ -1,11 +1,11 @@
 package org.dandelion.radiot.podcasts.download;
 
-import android.app.*;
 import android.app.DownloadManager;
 import org.dandelion.radiot.R;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import org.dandelion.radiot.util.IconNote;
 
 import java.io.File;
 
@@ -21,95 +21,50 @@ public class DownloadNotificationManager implements NotificationManager {
 
     @Override
     public void showSuccess(String title, File audioFile) {
-        Note note = new SuccessNote(context, title, audioFile);
-        note.show(title, DOWNLOAD_COMPLETE_NOTE_ID);
+        String text = context.getString(R.string.download_complete_message);
+        new SuccessNote(context, DOWNLOAD_COMPLETE_NOTE_ID, audioFile)
+                .setTitle(title)
+                .setText(text)
+                .setIcon(R.drawable.stat_download_complete)
+                .show(title);
     }
 
     @Override
     public void showError(String title, int errorCode) {
-        String message = errorMessages.getMessageForCode(errorCode);
-        Note note = new ErrorNote(context, title, message);
-        note.show(title, DOWNLOAD_COMPLETE_NOTE_ID);
+        String text = errorMessages.getMessageForCode(errorCode);
+        new ErrorNote(context, DOWNLOAD_COMPLETE_NOTE_ID)
+                .setTitle(title)
+                .setText(text)
+                .setIcon(android.R.drawable.stat_sys_warning)
+                .show(title);
+    }
+    class ErrorNote extends IconNote {
+
+        public ErrorNote(Context context, int notificationId) {
+            super(context, notificationId);
+        }
+
+        @Override
+        protected Intent activityIntent() {
+            return new Intent(DownloadManager.ACTION_VIEW_DOWNLOADS);
+        }
+    }
+
+    class SuccessNote extends IconNote {
+        private File path;
+
+        SuccessNote(Context context, int notificationId, File path) {
+            super(context, notificationId);
+            this.path = path;
+        }
+
+        @Override
+        protected Intent activityIntent() {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(Uri.fromFile(path), "audio/mpeg");
+            return intent;
+        }
     }
 }
 
-abstract class Note {
-    protected Context context;
-    private String title;
-
-    public Note(Context context, String title) {
-        this.context = context;
-        this.title = title;
-    }
-
-    public void show(String tag, int id) {
-        android.app.NotificationManager manager = (android.app.NotificationManager)
-                context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-        manager.notify(tag, id, build());
-    }
-
-    private Notification build() {
-        Notification note = new Notification(icon(),
-                title, System.currentTimeMillis());
-        note.setLatestEventInfo(context, title, text(), intent());
-        note.flags |= Notification.FLAG_AUTO_CANCEL;
-        return note;
-    }
-
-    public abstract CharSequence text();
-    public abstract int icon();
-    public abstract PendingIntent intent();
-}
-
-class ErrorNote extends Note {
-    private String message;
-
-    ErrorNote(Context context, String title, String message) {
-        super(context, title);
-        this.message = message;
-    }
-
-    @Override
-    public CharSequence text() {
-        return message;
-    }
-
-    @Override
-    public int icon() {
-        return android.R.drawable.stat_sys_warning;
-    }
-
-    @Override
-    public PendingIntent intent() {
-        Intent intent = new Intent(DownloadManager.ACTION_VIEW_DOWNLOADS);
-        return PendingIntent.getActivity(context, 0, intent, 0);
-    }
-}
-
-class SuccessNote extends Note {
-    private File path;
-
-    SuccessNote(Context context, String title, File path) {
-        super(context, title);
-        this.path = path;
-    }
-
-    @Override
-    public int icon() {
-        return R.drawable.stat_download_complete;
-    }
-
-    @Override
-    public CharSequence text() {
-        return context.getString(R.string.download_complete_message);
-    }
-
-    @Override
-    public PendingIntent intent() {
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setDataAndType(Uri.fromFile(path), "audio/mpeg");
-        return PendingIntent.getActivity(context, 0, intent, 0);
-    }
-}
 
---
+++ Radio-T/src/org/dandelion/radiot/util/IconNote.java
@@ -0,0 +1,54 @@
+package org.dandelion.radiot.util;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+public abstract class IconNote {
+    protected Context context;
+    private String title;
+    private String text;
+    private int iconId;
+    private int notificationId;
+
+    public IconNote(Context context, int notificationId) {
+        this.context = context;
+        this.notificationId = notificationId;
+    }
+
+    public IconNote setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public IconNote setText(String value) {
+        this.text = value;
+        return this;
+    }
+
+    public IconNote setIcon(int resourceId) {
+        this.iconId = resourceId;
+        return this;
+    }
+
+    public void show(String tag) {
+        android.app.NotificationManager manager = (android.app.NotificationManager)
+                context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        manager.notify(tag, notificationId, build());
+    }
+
+    private Notification build() {
+        Notification note = new Notification(iconId, title, System.currentTimeMillis());
+        note.setLatestEventInfo(context, title, text, intent());
+        note.flags |= Notification.FLAG_AUTO_CANCEL;
+        return note;
+    }
+
+    private PendingIntent intent() {
+        return PendingIntent.getActivity(context, 0, activityIntent(), 0);
+    }
+
+    protected abstract Intent activityIntent();
+}
