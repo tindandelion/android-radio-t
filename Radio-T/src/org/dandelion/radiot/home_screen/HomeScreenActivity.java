37e2b03d0b955a994fe2f1fa15d78bb56b19f290
Sergey Moshnikov <s.moshnikov@gmail.com>
2012-08-25 22:51:33 +0400

    Introduced DashboardFragment

--- Radio-T/src/org/dandelion/radiot/home_screen/HomeScreenActivity.java
+++ Radio-T/src/org/dandelion/radiot/home_screen/HomeScreenActivity.java
@@ -4,10 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.view.View;
 import org.dandelion.radiot.R;
-import org.dandelion.radiot.live.ui.LiveShowActivity;
-import org.dandelion.radiot.podcasts.ui.PodcastListActivity;
 
 public class HomeScreenActivity extends FragmentActivity {
 
@@ -20,41 +17,5 @@ public class HomeScreenActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
-        initDashboardItems();
-    }
-
-    private void initDashboardItems() {
-        onButtonClick(R.id.home_btn_main_show, new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                PodcastListActivity.start(HomeScreenActivity.this,
-                        getString(R.string.main_show_title), "main-show");
-            }
-        });
-        onButtonClick(R.id.home_btn_after_show, new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                PodcastListActivity.start(HomeScreenActivity.this,
-                        getString(R.string.after_show_title), "after-show");
-            }
-        });
-        onButtonClick(R.id.home_btn_live_show, new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(HomeScreenActivity.this,
-                        LiveShowActivity.class));
-            }
-        });
-        onButtonClick(R.id.home_btn_about, new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(HomeScreenActivity.this,
-                        AboutAppActivity.class));
-            }
-        });
-    }
-
-    private void onButtonClick(int id, View.OnClickListener listener) {
-        findViewById(id).setOnClickListener(listener);
     }
 }
