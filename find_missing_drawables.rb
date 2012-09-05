8ece50759cf6baee085d9542ef38f012c37919be
Sergey Moshnikov <s.moshnikov@gmail.com>
2012-09-05 08:35:38 +0400

    Script is ready

--- find_missing_drawables.rb
+++ find_missing_drawables.rb
@@ -12,3 +12,23 @@ def mdpi_directories
   end
 end
 
+def version_suffix_from(path)
+  components = path.basename.to_s.split('-')
+  return "" if components.size < 3
+  return '-' + components.last
+end
+
+def xhdpi_directory(version_suffix)
+  RES_DIR.join(XHDPI_DIR_PREFIX + version_suffix)
+end
+
+mdpi_directories.each do |path|
+  version_suffix = version_suffix_from(path)
+  xhdpi_dir = xhdpi_directory(version_suffix)
+  puts xhdpi_dir
+  path.children.each do |file|
+    resource_name = file.basename.to_s
+    xhdpi_file = xhdpi_dir + resource_name
+    puts "\t" + resource_name unless xhdpi_file.exist?
+  end
+end
