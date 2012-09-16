2ae3086beb95852d32da0f323daba3de9b63a199
Sergey Moshnikov <s.moshnikov@gmail.com>
2012-09-16 12:59:56 +0400

    Empty list is shown

--- Radio-T/src/org/dandelion/radiot/RadiotApplication.java
+++ Radio-T/src/org/dandelion/radiot/RadiotApplication.java
@@ -8,38 +8,47 @@ import org.dandelion.radiot.podcasts.core.PodcastList.IPodcastListEngine;
 import java.util.HashMap;
 
 public class RadiotApplication extends Application {
-	private HashMap<String, IPodcastListEngine> engines;
+    private HashMap<String, IPodcastListEngine> engines;
 
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		engines = new HashMap<String, IPodcastListEngine>();
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        engines = new HashMap<String, IPodcastListEngine>();
         engines.put("main-show",
-				createPodcastEngine("http://feeds.rucast.net/radio-t", new HttpThumbnailDownloader()));
+                podcastEngine("http://feeds.rucast.net/radio-t", new HttpThumbnailDownloader()));
         engines.put(
                 "after-show",
-                createPodcastEngine("http://feeds.feedburner.com/pirate-radio-t", new NullThumbnailDownloader()));
+                podcastEngine("http://feeds.feedburner.com/pirate-radio-t", new NullThumbnailDownloader()));
         engines.put(
                 "test-show",
-                createPodcastEngine("http://localhost:8080/rss", new NullThumbnailDownloader()));
+                testPodcastEngine());
         PodcastsApp.initialize(this);
-	}
-	
-	@Override
-	public void onTerminate() {
-		super.onTerminate();
-        PodcastsApp.release();
-	}
+    }
 
-	protected IPodcastListEngine createPodcastEngine(String url, PodcastList.ThumbnailDownloader thumbnailDownloader) {
-		return new PodcastListEngine(new RssFeedModel(url, thumbnailDownloader));
-	}
+    private IPodcastListEngine testPodcastEngine() {
+        return new PodcastListEngine(new RssFeedModel("http://localhost:8080/rss", new NullThumbnailDownloader())) {
+            @Override
+            public void refresh(boolean resetCache) {
+                super.refresh(true);
+            }
+        };
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        PodcastsApp.release();
+    }
+
+    protected IPodcastListEngine podcastEngine(String url, PodcastList.ThumbnailDownloader thumbnailDownloader) {
+        return new PodcastListEngine(new RssFeedModel(url, thumbnailDownloader));
+    }
 
     public IPodcastListEngine getPodcastEngine(String name) {
-		return engines.get(name);
-	}
+        return engines.get(name);
+    }
 
-	public void setPodcastEngine(String name, IPodcastListEngine engine) {
-		engines.put(name, engine);
-	}
+    public void setPodcastEngine(String name, IPodcastListEngine engine) {
+        engines.put(name, engine);
+    }
 }
