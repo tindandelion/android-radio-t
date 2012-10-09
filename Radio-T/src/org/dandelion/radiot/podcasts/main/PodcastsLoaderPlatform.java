2aa9fdae9a858aebfcd9ffe16b3eb9ce15944022
Sergey Moshnikov <s.moshnikov@gmail.com>
2012-10-09 07:50:20 +0400

    Moved PodcastProperties into the PodcastLoaderPlatform

--- Radio-T/src/org/dandelion/radiot/podcasts/main/PodcastsLoaderPlatform.java
+++ Radio-T/src/org/dandelion/radiot/podcasts/main/PodcastsLoaderPlatform.java
@@ -4,10 +4,22 @@ import android.content.Context;
 import org.dandelion.radiot.podcasts.loader.*;
 
 import java.io.File;
+import java.util.HashMap;
 
 public class PodcastsLoaderPlatform implements LoaderFactory {
     private static final int CACHE_FORMAT_VERSION = 5;
     private static final String THUMBNAIL_HOST = "http://www.radio-t.com";
+
+    private static HashMap<String, PodcastProperties> shows;
+    static {
+        shows = new HashMap<String, PodcastProperties>();
+        shows.put("main-show",
+                new PodcastProperties("main-show",
+                        "http://feeds.rucast.net/radio-t"));
+        shows.put("after-show",
+                new PodcastProperties("after-show",
+                        "http://feeds.feedburner.com/pirate-radio-t"));
+    }
 
     private Context context;
 
@@ -17,8 +29,12 @@ public class PodcastsLoaderPlatform implements LoaderFactory {
 
     @Override
     public PodcastListLoader createLoaderForShow(String name) {
-        PodcastProperties props = PodcastProperties.propertiesForShow(name);
+        PodcastProperties props = propertiesForShow(name);
         return createPodcastLoader(props);
+    }
+
+    private PodcastProperties propertiesForShow(String name) {
+        return shows.get(name);
     }
 
     private PodcastListLoader createPodcastLoader(PodcastProperties props) {
@@ -31,5 +47,15 @@ public class PodcastsLoaderPlatform implements LoaderFactory {
     private PodcastsCache createPodcastsCache(String name) {
         File cacheFile = new File(context.getCacheDir(), name);
         return new FilePodcastsCache(cacheFile, CACHE_FORMAT_VERSION);
+    }
+
+    private static class PodcastProperties {
+        public String name;
+        public String url;
+
+        public PodcastProperties(String name, String url) {
+            this.name = name;
+            this.url = url;
+        }
     }
 }
