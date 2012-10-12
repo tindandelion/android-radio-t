9b7269accc019fded54494039614e003aa735ea0
Sergey Moshnikov <s.moshnikov@gmail.com>
2012-10-12 07:58:09 +0400

    LiveShowApp acceptance tests now use local streaming server

--- Radio-T-Test/src/org/dandelion/radiot/accepttest/testables/TestableMediaPlayerStream.java
+++ Radio-T-Test/src/org/dandelion/radiot/accepttest/testables/TestableMediaPlayerStream.java
@@ -1,29 +1,23 @@
 package org.dandelion.radiot.accepttest.testables;
 
-import android.util.Log;
 import org.dandelion.radiot.live.MediaPlayerStream;
 
 import java.io.IOException;
 
 public class TestableMediaPlayerStream extends MediaPlayerStream {
-    private static String ACTIVE_TRANSLATION_URL = "http://174.37.110.72:5020";
     private static String INACTIVE_TRANSLATION_URL = "http://non-existent";
 
     private String url = "";
+    private String validUrl;
 
-    public TestableMediaPlayerStream() {
+    public TestableMediaPlayerStream(String validUrl) {
         super(null);
+        this.validUrl = validUrl;
     }
 
     @Override
     public void play() throws IOException {
         playUrl(url);
-    }
-
-    @Override
-    protected void playUrl(String url) throws IOException {
-        Log.d("LIVE", "Playing URL: " + url);
-        super.playUrl(url);
     }
 
     @Override
@@ -40,6 +34,6 @@ public class TestableMediaPlayerStream extends MediaPlayerStream {
     }
 
     public void activateTranslation() {
-        url = ACTIVE_TRANSLATION_URL;
+        url = validUrl;
     }
 }
