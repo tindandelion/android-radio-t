bb16b285a58ccf2e3758fff01cd20c99c89f7e6a
Sergey Moshnikov <s.moshnikov@gmail.com>
2012-10-12 08:43:36 +0400

    Revert "LiveShowApp acceptance tests now use local streaming server"
    This reverts commit 9b7269accc019fded54494039614e003aa735ea0.

--- Radio-T-Test/src/org/dandelion/radiot/accepttest/testables/TestableMediaPlayerStream.java
+++ Radio-T-Test/src/org/dandelion/radiot/accepttest/testables/TestableMediaPlayerStream.java
@@ -1,23 +1,29 @@
 package org.dandelion.radiot.accepttest.testables;
 
+import android.util.Log;
 import org.dandelion.radiot.live.MediaPlayerStream;
 
 import java.io.IOException;
 
 public class TestableMediaPlayerStream extends MediaPlayerStream {
+    private static String ACTIVE_TRANSLATION_URL = "http://174.37.110.72:5020";
     private static String INACTIVE_TRANSLATION_URL = "http://non-existent";
 
     private String url = "";
-    private String validUrl;
 
-    public TestableMediaPlayerStream(String validUrl) {
+    public TestableMediaPlayerStream() {
         super(null);
-        this.validUrl = validUrl;
     }
 
     @Override
     public void play() throws IOException {
         playUrl(url);
+    }
+
+    @Override
+    protected void playUrl(String url) throws IOException {
+        Log.d("LIVE", "Playing URL: " + url);
+        super.playUrl(url);
     }
 
     @Override
@@ -34,6 +40,6 @@ public class TestableMediaPlayerStream extends MediaPlayerStream {
     }
 
     public void activateTranslation() {
-        url = validUrl;
+        url = ACTIVE_TRANSLATION_URL;
     }
 }
--- Radio-T-Test/src/org/dandelion/radiot/integration/MediaPlayerTest.java
+++ Radio-T-Test/src/org/dandelion/radiot/integration/MediaPlayerTest.java
@@ -1,15 +1,21 @@
 package org.dandelion.radiot.integration;
 
+import android.content.Context;
 import android.media.MediaPlayer;
 import android.test.InstrumentationTestCase;
-import org.dandelion.radiot.helpers.AudioStreamServer;
+import org.dandelion.radiot.helpers.NanoHTTPD;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class MediaPlayerTest extends InstrumentationTestCase {
-    private AudioStreamServer backend;
+    private StreamServer backend;
     private MediaPlayer player;
 
     public void testConnection() throws Exception {
-        player.setDataSource(AudioStreamServer.MAIN_URL);
+        player.setDataSource(StreamServer.REDIRECT_URL);
         player.prepare();
         player.start();
         Thread.sleep(3000);
@@ -18,7 +24,7 @@ public class MediaPlayerTest extends InstrumentationTestCase {
     @Override
     public void setUp() throws Exception {
         super.setUp();
-        backend = new AudioStreamServer(getInstrumentation().getContext());
+        backend = new StreamServer(getInstrumentation().getContext());
         player = new MediaPlayer();
     }
 
@@ -29,4 +35,46 @@ public class MediaPlayerTest extends InstrumentationTestCase {
         super.tearDown();
     }
 
+    private static class StreamServer extends NanoHTTPD {
+        public static int PORT = 32768;
+        public static String STREAM_RESOURCE = "/stream";
+        private static final String MAIN_RESOURCE = "/main";
+        public static String DIRECT_URL = String.format("http://localhost:%d", PORT) + STREAM_RESOURCE;
+        public static String REDIRECT_URL = String.format("http://localhost:%d", PORT) + MAIN_RESOURCE;
+        private Context context;
+
+        public StreamServer(Context context) throws IOException {
+            super(PORT, new File(""));
+            this.context = context;
+        }
+
+        @Override
+        public Response serve(String uri, String method, Properties header, Properties parms, Properties files) {
+            if (uri.equals(MAIN_RESOURCE)) {
+                return redirectToStream();
+            }
+            if (uri.equals(STREAM_RESOURCE)) {
+                return respondWithStream();
+            }
+            return null;
+        }
+
+        private Response redirectToStream() {
+            Response response = new Response("302 Found", MIME_HTML, "");
+            response.addHeader("Location", DIRECT_URL);
+            return response;
+        }
+
+        private Response respondWithStream() {
+            try {
+                return new Response(HTTP_OK, "audio/mpeg", openAudioStream());
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        private InputStream openAudioStream() throws IOException {
+            return context.getAssets().open("stream.mp3");
+        }
+    }
 }
