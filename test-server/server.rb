e04de66a080bc018339021e45c63698b14d60efe
Sergey Moshnikov <s.moshnikov@gmail.com>
2012-10-15 07:36:08 +0400

    Updated test server to simulate network delays

--- test-server/server.rb
+++ test-server/server.rb
@@ -1,23 +1,82 @@
 require "sinatra"
 require "pathname"
+require "pry"
 
-STREAM_FILE = Pathname(__FILE__).dirname + "stream.mp3"
-DELAY_BETWEEN_CHUNKS = 0.102
+STREAM_FILE = Pathname(__FILE__).dirname + "stream.mp3" 
+# STREAM_FILE = Pathname(__FILE__).dirname + "stream-small.mp3"
+
+CHUNK_SIZE = 4096
+DELAY_MEAN = 0.102
+DELAY_VARIATION = 0.05
+SECOND_DELAY_CHANCE = 0.01
+GRAND_DELAY_CHANCE = 0.001
+GRAND_DELAY = 40
+
+
+class Stat
+  def initialize
+    @bytes_count = 0
+  end
+
+  def start
+    @bytes_count = 0
+    @start_time = Time.now
+  end
+
+  def stop
+    @stop_time = Time.now
+  end
+
+  def sample(count)
+    @bytes_count += count
+  end
+
+  def bytes_per_sec
+    @bytes_count / (@stop_time - @start_time)
+  end
+end
 
 get "/stream" do
-  puts "Starting streaming with delays: #{DELAY_BETWEEN_CHUNKS}"
+  puts "\n * Starting streaming with delay mean: #{DELAY_MEAN}"
   content_type "audio/mpeg"
   stream :keep_open do |out|
-    io = open(STREAM_FILE, "rb")
-    read_file_with_delays io, out, DELAY_BETWEEN_CHUNKS
+    open(STREAM_FILE, "rb") { |io| read_file_with_delays io, out, DELAY_MEAN }
   end
 end
 
 def read_file_with_delays(io, out, delay)
-  while not io.eof?
-    out << io.readpartial(4096)
+  stat = Stat.new
+  stat.start
+  interrupted = false
+
+  out.callback { interrupted = true }
+
+  while not (io.eof? or interrupted)
+    chunk = io.readpartial(4096)
+    stat.sample chunk.size
+    out << chunk
     print "."
-    sleep delay
+    sleep randomize(delay)
   end
+  stat.stop
+  out.close
+  puts "\nStatistics is: #{stat.bytes_per_sec.to_i}"
 end
 
+def randomize(median)
+  fluctuated = median + fluctuation + second_delay + grand_delay
+end
+
+def second_delay
+  chances = rand()
+  (chances < SECOND_DELAY_CHANCE) ? 1 : 0
+end
+
+def grand_delay
+  chances = rand()
+  (chances < GRAND_DELAY_CHANCE) ? GRAND_DELAY : 0
+end
+
+def fluctuation
+  ((DELAY_VARIATION / 2) - (DELAY_VARIATION * rand()))
+end
