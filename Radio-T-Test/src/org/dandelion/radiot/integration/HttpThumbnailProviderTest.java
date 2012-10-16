ca87a651dc2a4c7cbe46564ebda8ceb820b73355
Sergey Moshnikov <s.moshnikov@gmail.com>
2012-10-16 06:04:34 +0400

    Minor changes

--- Radio-T-Test/src/org/dandelion/radiot/integration/HttpThumbnailProviderTest.java
+++ Radio-T-Test/src/org/dandelion/radiot/integration/HttpThumbnailProviderTest.java
@@ -9,34 +9,34 @@ import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class HttpThumbnailProviderTest extends TestCase {
-    private HttpThumbnailProvider downloader;
+    private HttpThumbnailProvider thumbnails;
 
     @Override
     public void setUp() throws Exception {
-        downloader = new HttpThumbnailProvider("http://www.radio-t.com");
+        thumbnails = new HttpThumbnailProvider("http://www.radio-t.com");
     }
 
     public void testDownloadPodcastByFullUrl() throws Exception {
         final String fullUrl = "http://www.radio-t.com/images/radio-t/rt302.jpg";
-        assertThat(downloader.thumbnailDataFor(fullUrl), is(notNullValue()));
+        assertThat(thumbnails.thumbnailDataFor(fullUrl), is(notNullValue()));
     }
 
     public void testDownloadPodcastByPartialUrl() throws Exception {
         final String partialUrl = "/images/radio-t/rt302.jpg";
-        assertThat(downloader.thumbnailDataFor(partialUrl), is(notNullValue()));
+        assertThat(thumbnails.thumbnailDataFor(partialUrl), is(notNullValue()));
     }
 
     public void testReturnNullIfUnableToLoadThumbnail() throws Exception {
         final String nonExistentUrl = "/non-existent/thumbnail.jpg";
-        assertThat(downloader.thumbnailDataFor(nonExistentUrl), is(nullValue()));
+        assertThat(thumbnails.thumbnailDataFor(nonExistentUrl), is(nullValue()));
     }
 
     public void testReturnNullIfNowUrlIsSupplied() throws Exception {
-        assertThat(downloader.thumbnailDataFor(null), is(nullValue()));
+        assertThat(thumbnails.thumbnailDataFor(null), is(nullValue()));
     }
 
     public void testJunkUri() throws Exception {
         String junkUrl = "http://ftp://Hello world/This is junk";
-        assertThat(downloader.thumbnailDataFor(junkUrl), is(nullValue()));
+        assertThat(thumbnails.thumbnailDataFor(junkUrl), is(nullValue()));
     }
 }
