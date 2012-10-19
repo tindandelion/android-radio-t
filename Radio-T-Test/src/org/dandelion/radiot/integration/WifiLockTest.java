7a3a4957d880ea5315e1c321ac97dd07f9c3fbdd
Sergey Moshnikov <sergey.moshnikov@f-secure.com>
2012-10-19 12:56:56 +0400

    Added a test for WifiLock with unsupported type

---
+++ Radio-T-Test/src/org/dandelion/radiot/integration/WifiLockTest.java
@@ -0,0 +1,28 @@
+package org.dandelion.radiot.integration;
+
+import android.content.Context;
+import android.net.wifi.WifiManager;
+import android.test.InstrumentationTestCase;
+
+public class WifiLockTest extends InstrumentationTestCase {
+
+    public void testLockWithUnsupportedType_ShouldNotCauseErrors() throws Exception {
+        final int UNSUPPORTED_TYPE = 10;
+        WifiManager.WifiLock lock = createWifiLockWithType(UNSUPPORTED_TYPE);
+
+        assertNotNull(lock);
+        try {
+            lock.acquire();
+            lock.release();
+        } catch (Exception ex) {
+            fail(ex.getMessage());
+        }
+    }
+
+
+    private WifiManager.WifiLock createWifiLockWithType(int type) {
+        Context context = getInstrumentation().getContext();
+        WifiManager manager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        return manager.createWifiLock(type, "TestLock");
+    }
+}
