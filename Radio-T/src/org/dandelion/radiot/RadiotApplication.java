3aea51282b464dde57525c00845899ddd78a8edb
Sergey Moshnikov <s.moshnikov@gmail.com>
2012-11-24 10:05:58 +0400

    Creating a chat in production

--- Radio-T/src/org/dandelion/radiot/RadiotApplication.java
+++ Radio-T/src/org/dandelion/radiot/RadiotApplication.java
@@ -1,13 +1,22 @@
 package org.dandelion.radiot;
 
 import android.app.Application;
+import org.dandelion.radiot.live.ui.ChatTranslationFragment;
+import org.dandelion.radiot.live.ui.HttpChatTranslation;
 import org.dandelion.radiot.podcasts.main.PodcastsApp;
 
 public class RadiotApplication extends Application {
+    private static final String CHAT_URL = "http://chat.radio-t.com:18000";
+
     @Override
     public void onCreate() {
         super.onCreate();
         PodcastsApp.initialize(this);
+        setupChatTranslation();
+    }
+
+    private void setupChatTranslation() {
+        ChatTranslationFragment.chat = new HttpChatTranslation(CHAT_URL);
     }
 
     @Override
