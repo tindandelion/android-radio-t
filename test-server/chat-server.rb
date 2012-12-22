897cb474b38b54724c450843260e4d2aa1b71a1b
Sergey Moshnikov <s.moshnikov@gmail.com>
2012-12-22 12:32:20 +0400

    Extending test chat server

--- test-server/chat-server.rb
+++ test-server/chat-server.rb
@@ -1,6 +1,7 @@
 require "sinatra"
 require 'json'
 require 'faker'
+require 'pry'
 
 MESSAGE_TEXT = <<-EOM
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
@@ -10,32 +11,38 @@ EOM
 
 get '/data/jsonp' do
   content_type 'application/json'
-  compose_chat_stream
-end
-
-def compose_chat_stream
-  wrap_into_callback compose_chat_json
+  msg_count = (request.params['mode'] == 'last') ? 10 : rand(5)
+  wrap_into_callback compose_chat_json(msg_count)
 end
 
 def wrap_into_callback(json)
   "callback_fn(#{json})"
 end
 
-def compose_chat_json
-  { "records" => initial_records_array }.to_json
+def compose_chat_json(count)
+  { "records" => records_array(count) }.to_json
 end
 
-def initial_records_array
-  (1..5).collect { |i| create_chat_record }
+def records_array(count)
+  (1..count).collect { |i| create_chat_record }
 end
 
 def create_chat_record
   {
-    'msg' => Faker::Lorem.sentence,
+    'msg' => generate_message,
     'from' => Faker::Name.name,
     'time' => 'Sat Dec 15 22:12:10 UTC 2012'
   }
 end
+
+def generate_message
+  if rand < 0.3
+    Faker::Lorem.paragraph
+  else
+    Faker::Lorem.sentence
+  end
+end
+
 
 
 
