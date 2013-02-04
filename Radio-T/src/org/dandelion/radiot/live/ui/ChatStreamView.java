d94bef3dc8af407ebfdd853ffb2f722acff0ce7f
Sergey Moshnikov <s.moshnikov@gmail.com>
2013-02-04 06:57:55 +0400

    ChatStreamView scrolling support for Android 2.1

--- Radio-T/src/org/dandelion/radiot/live/ui/ChatStreamView.java
+++ Radio-T/src/org/dandelion/radiot/live/ui/ChatStreamView.java
@@ -1,10 +1,16 @@
 package org.dandelion.radiot.live.ui;
 
 import android.content.Context;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.widget.ListView;
 
 public class ChatStreamView extends ListView {
+    private static boolean useSmoothScroll = true;
+    static {
+        useSmoothScroll = Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO;
+    }
+
     public ChatStreamView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
@@ -14,6 +20,11 @@ public class ChatStreamView extends ListView {
     }
 
     public void scrollToBottom() {
-        smoothScrollToPosition(getCount());
+        int lastPosition = getCount() - 1;
+        if (useSmoothScroll) {
+            smoothScrollToPosition(lastPosition);
+        } else {
+            setSelection(lastPosition);
+        }
     }
 }
