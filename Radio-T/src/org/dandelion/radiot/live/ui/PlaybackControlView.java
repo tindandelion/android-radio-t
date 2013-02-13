b7eea12d0ed516f27873ca9dc92d3263578d3031
Sergey Moshnikov <s.moshnikov@gmail.com>
2013-02-13 12:34:18 +0400

    PlaybackControlView is inherited from RelativeLayout

--- Radio-T/src/org/dandelion/radiot/live/ui/PlaybackControlView.java
+++ Radio-T/src/org/dandelion/radiot/live/ui/PlaybackControlView.java
@@ -4,13 +4,13 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import org.dandelion.radiot.R;
 import org.dandelion.radiot.live.core.LiveShowState;
 import org.dandelion.radiot.live.core.LiveShowStateListener;
 
-public class PlaybackControlView extends LinearLayout implements LiveShowStateListener {
+public class PlaybackControlView extends RelativeLayout implements LiveShowStateListener {
 
     private final String[] statusLabels;
 
