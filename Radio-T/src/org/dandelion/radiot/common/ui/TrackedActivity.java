2b24e71032f17d9def96ff04f73e35f9c6115bf8
Sergey Moshnikov <s.moshnikov@gmail.com>
2013-03-07 06:24:35 +0400

    Cleaning up

--- Radio-T/src/org/dandelion/radiot/common/ui/TrackedActivity.java
+++ Radio-T/src/org/dandelion/radiot/common/ui/TrackedActivity.java
@@ -1,18 +1,27 @@
 package org.dandelion.radiot.common.ui;
 
+import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import com.google.analytics.tracking.android.EasyTracker;
 
 public class TrackedActivity extends FragmentActivity {
+    private EasyTracker tracker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        tracker = EasyTracker.getInstance();
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
-        EasyTracker.getInstance().activityStart(this);
+        tracker.activityStart(this);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        EasyTracker.getInstance().activityStop(this);
+        tracker.activityStop(this);
     }
 }
