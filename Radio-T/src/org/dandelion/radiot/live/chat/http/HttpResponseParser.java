7ed31326a12213057573bc20fe0e4fb699461b14
Sergey Moshnikov <s.moshnikov@gmail.com>
2014-01-26 11:22:36 +0400

    Cosmetical changes in HttpResponseParser

--- Radio-T/src/org/dandelion/radiot/live/chat/http/HttpResponseParser.java
+++ Radio-T/src/org/dandelion/radiot/live/chat/http/HttpResponseParser.java
@@ -4,7 +4,6 @@ import org.dandelion.radiot.live.chat.Message;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.JSONTokener;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -13,30 +12,26 @@ import java.util.Date;
 import java.util.List;
 
 public class HttpResponseParser {
-    private final String source;
     private static final SimpleDateFormat TIME_FORMAT = new SimpleDateFormat("HH:mm");
     private static final SimpleDateFormat TIMESTAMP_FORMAT =
             new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy");
 
     public static List<Message> parse(String strJson) throws JSONException {
-        HttpResponseParser parser = new HttpResponseParser(strJson);
-        return parser.getMessages();
-    }
+        JSONArray records = getMessageArray(strJson);
 
-    HttpResponseParser(String strJson) {
-        this.source = strJson;
-    }
-
-    private List<Message> getMessages() throws JSONException {
         ArrayList<Message> messages = new ArrayList<Message>();
-        JSONArray records = getRecords();
         for (int i = 0; i < records.length(); i++) {
             messages.add(newMessage(records.getJSONObject(i)));
         }
         return messages;
     }
 
-    private Message newMessage(JSONObject record) throws JSONException {
+    private static JSONArray getMessageArray(String strJson) throws JSONException {
+        JSONObject json = new JSONObject(strJson);
+        return json.getJSONArray("msgs");
+    }
+
+    private static Message newMessage(JSONObject record) throws JSONException {
         return new Message(
                 record.getString("from"),
                 record.getString("msg"),
@@ -44,7 +39,7 @@ public class HttpResponseParser {
                 record.getInt("seq"));
     }
 
-    private String formatTime(String timestamp) {
+    private static String formatTime(String timestamp) {
         try {
             Date ts = TIMESTAMP_FORMAT.parse(timestamp);
             return TIME_FORMAT.format(ts);
@@ -53,12 +48,4 @@ public class HttpResponseParser {
         }
 
     }
-
-    private JSONArray getRecords() throws JSONException {
-        JSONTokener tokener = new JSONTokener(source);
-        tokener.skipTo('{');
-        JSONObject json = new JSONObject(tokener);
-        return json.getJSONArray("msgs");
-    }
-
 }
