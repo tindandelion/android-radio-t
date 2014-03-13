00ad499a93ac4030a97676e0273a5498ae1c512b
Sergey Moshnikov <s.moshnikov@gmail.com>
2014-03-13 09:31:08 +0400

    Added web.xml and infrastructure classes, yet unable to run it in Jetty

---
+++ radio-t-server/src/main/scala/ScalatraBootstrap.scala
@@ -0,0 +1,8 @@
+import org.scalatra._
+import javax.servlet.ServletContext
+
+class ScalatraBootstrap extends LifeCycle {
+  override def init(context: ServletContext) {
+    context.mount(new HelloWorldServlet, "/*")
+  }
+}
