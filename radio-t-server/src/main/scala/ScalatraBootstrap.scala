d2f537ee2520fe56e67d5119ee34a1155297c945
Sergey Moshnikov <s.moshnikov@gmail.com>
2014-03-14 23:23:13 +0400

    Created a JettyLauncher class to run application with embedded Jetty container

--- radio-t-server/src/main/scala/ScalatraBootstrap.scala
+++ radio-t-server/src/main/scala/ScalatraBootstrap.scala
@@ -3,6 +3,6 @@ import javax.servlet.ServletContext
 
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
-    context.mount(new HelloWorldServlet, "/*")
+    context.mount(new HelloWorldServlet, "/chat/*")
   }
 }
