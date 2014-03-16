78934d00332bd98d8964a9c53b50692914b3ca1b
Sergey Moshnikov <s.moshnikov@gmail.com>
2014-03-16 09:16:37 +0400

    Joining the chat

--- radio-t-server/src/test/scala/TopicTrackerTest.scala
+++ radio-t-server/src/test/scala/TopicTrackerTest.scala
@@ -1,28 +1,26 @@
 import org.jivesoftware.smack.{ConnectionConfiguration, XMPPConnection}
-import org.scalatest.{Matchers, FunSpec}
+import org.jivesoftware.smackx.muc.MultiUserChat
+import org.scalatest.{BeforeAndAfter, Matchers, FunSpec}
 
-class TopicTrackerTest extends FunSpec with Matchers {
-  it("passes") {
-    1 should equal(1)
-  }
+class TopicTrackerTest extends FunSpec with Matchers with BeforeAndAfter {
+  val config = new ConnectionConfiguration("jabber.org", 5222)
+  val connection = new XMPPConnection(config)
 
-  it("connects to the XMPP server") {
-    val config = new ConnectionConfiguration("jabber.org", 5222)
-    val connection = new XMPPConnection(config)
-
+  before {
     connection.connect()
     connection.login("android-radiot@jabber.org", "android-radiot")
+  }
 
+  after {
     connection.disconnect()
   }
 
   it("joins the chat") {
-    val config = new ConnectionConfiguration("jabber.org", 5222)
-    val connection = new XMPPConnection(config)
+    val chat = new MultiUserChat(connection, "online@conference.radio-t.com")
 
-    connection.connect()
-    connection.login("android-radiot@jabber.org", "android-radiot")
+    chat.join("android-radiot")
+    chat should be('joined)
 
-    connection.disconnect()
+    chat.leave()
   }
 }
