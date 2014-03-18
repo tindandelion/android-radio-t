6e891adbcc9338e0dc12a931b0f70246c4101a50
Sergey Moshnikov <s.moshnikov@gmail.com>
2014-03-18 22:06:59 +0400

    Written a test that tests the connection to the WebSocket server

--- radio-t-server/src/test/scala/org/dandelion/radiot/server/TopicTrackerServletTest.scala
+++ radio-t-server/src/test/scala/org/dandelion/radiot/server/TopicTrackerServletTest.scala
@@ -2,8 +2,41 @@ package org.dandelion.radiot.server
 
 import org.scalatest.Matchers
 import org.scalatra.test.scalatest.ScalatraSpec
+import org.eclipse.jetty.websocket.client.WebSocketClient
+import java.net.URI
+import org.eclipse.jetty.websocket.api.annotations._
+import org.eclipse.jetty.websocket.api.Session
+import org.scalatest.concurrent.Eventually
+import scala.concurrent.duration._
 
-class TopicTrackerServletTest extends ScalatraSpec with Matchers {
+@WebSocket
+class SimpleEchoSocket {
+  var session: Session = null
+  var messages: List[String] = List()
+
+  @OnWebSocketConnect
+  def onConnect(session: Session) {
+    this.session = session
+  }
+
+  @OnWebSocketMessage
+  def onMessage(msg: String) {
+    messages = msg :: messages
+  }
+
+  @OnWebSocketError
+  def onError(error: Throwable) {
+    println("Error " + error.toString)
+  }
+
+  def isConnected = (session != null) && session.isOpen
+
+  def sendMessage(msg: String) = session.getRemote.sendString(msg)
+}
+
+class TopicTrackerServletTest extends ScalatraSpec with Matchers with Eventually {
+  implicit override def patienceConfig = PatienceConfig(timeout = 5 seconds)
+
   addServlet(new TopicTrackerServlet, "/*")
 
   it("answers a simple request") {
@@ -11,5 +44,20 @@ class TopicTrackerServletTest extends ScalatraSpec with Matchers {
       status should equal(200)
       body should equal("Hello world!")
     }
+  }
+
+  it("connects to a WebSocket server") {
+    val client = new WebSocketClient
+    val socket = new SimpleEchoSocket
+
+    client.start()
+    client.connect(socket, new URI("ws://echo.websocket.org"))
+    eventually { socket should be('connected) }
+
+    socket.sendMessage("Hello")
+    socket.sendMessage("World")
+
+    eventually { socket.messages should contain allOf("Hello", "World") }
+    client.stop()
   }
 }
