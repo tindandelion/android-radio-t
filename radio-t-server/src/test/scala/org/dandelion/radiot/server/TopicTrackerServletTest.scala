56a4a085fc8e6c7252c1f04aaa63dabc01d88a10
Sergey Moshnikov <s.moshnikov@gmail.com>
2014-04-02 08:03:51 +0400

    Some clean-up in tests

--- radio-t-server/src/test/scala/org/dandelion/radiot/server/TopicTrackerServletTest.scala
+++ radio-t-server/src/test/scala/org/dandelion/radiot/server/TopicTrackerServletTest.scala
@@ -6,6 +6,56 @@ import java.net.URI
 import org.eclipse.jetty.websocket.api.annotations._
 import org.eclipse.jetty.websocket.api.Session
 import org.scalatest.concurrent.Eventually
+import scala.concurrent.duration._
+
+class TopicTrackerServletTest extends RadiotServerSpec
+with Matchers with Eventually with BeforeAndAfter {
+  implicit val config = PatienceConfig(timeout = 5 seconds, interval = 0.5 seconds)
+
+  addServlet(new TopicTrackerServlet, "/chat/*")
+
+  val client = new WebSocketClient
+  val socket = new TopicTrackerSocket
+
+  before {
+    client.start()
+    client.connect(socket, serverUrl)
+
+    eventually { socket should be('connected) }
+  }
+
+  after {
+    client.stop()
+  }
+
+  it("answers a simple request") {
+    get("/chat") {
+      status should equal(200)
+      body should equal("Hello world!")
+    }
+  }
+
+  it("receives current topic after connection is established") {
+    topicShouldBe("Default topic")
+  }
+
+  it("changes a topic by POST request") {
+    val newTopic = "New topic"
+    post("/chat/set-topic", newTopic) {
+      status should equal(200)
+      topicShouldBe(newTopic)
+    }
+  }
+
+  def topicShouldBe(expected: String) {
+    eventually { socket.topic should equal(expected) }
+  }
+
+  def serverUrl = localPort match {
+    case Some(port) => new URI(s"ws://localhost:$port/chat/current-topic")
+    case None => throw new RuntimeException("No port is specified")
+  }
+}
 
 @WebSocket
 class TopicTrackerSocket {
@@ -26,46 +76,3 @@ class TopicTrackerSocket {
   def isConnected = (session != null) && session.isOpen
 }
 
-class TopicTrackerServletTest extends RadiotServerSpec
-with Matchers with Eventually with BeforeAndAfter {
-
-  addServlet(new TopicTrackerServlet, "/chat/*")
-
-  val client = new WebSocketClient
-  val socket = new TopicTrackerSocket
-
-  before {
-    client.start()
-    client.connect(socket, serverUrl)
-  }
-
-  after {
-    client.stop()
-  }
-
-  it("answers a simple request") {
-    get("/chat") {
-      status should equal(200)
-      body should equal("Hello world!")
-    }
-  }
-
-  it("receives current topic after connection is established") {
-    eventually { socket should be('connected) }
-    eventually { socket.topic should equal("Default topic") }
-  }
-
-  it("changes a topic by POST request") {
-    eventually { socket should be('connected) }
-
-    post("/chat/set-topic", "New topic") {
-      status should equal(200)
-      eventually { socket.topic should equal("New topic") }
-    }
-  }
-
-  def serverUrl = localPort match {
-    case Some(port) => new URI(s"ws://localhost:$port/chat/current-topic")
-    case None => throw new RuntimeException("No port is specified")
-  }
-}
