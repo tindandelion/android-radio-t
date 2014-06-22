00e72e4f898aa8a4aafa0c02c12defaf1feee103
Sergey Moshnikov <s.moshnikov@gmail.com>
2014-06-22 12:37:21 +0400

    Removed all WebSocket dependent stuff from the server

--- radio-t-server/src/main/scala/org/dandelion/radiot/server/TopicTrackerServlet.scala
+++ radio-t-server/src/main/scala/org/dandelion/radiot/server/TopicTrackerServlet.scala
@@ -1,46 +1,32 @@
 package org.dandelion.radiot.server
 
-import org.scalatra.{NoContent, Ok, SessionSupport, ScalatraServlet}
-import org.scalatra.atmosphere._
-import org.scalatra.json.{JValueResult, JacksonJsonSupport}
-import org.json4s.{DefaultFormats, Formats}
-import org.json4s.JsonDSL._
-
-import scala.concurrent._
-import ExecutionContext.Implicits.global
-import org.slf4j.LoggerFactory
 import org.json4s.JsonAST.JObject
+import org.json4s.JsonDSL._
+import org.json4s.{DefaultFormats, Formats}
+import org.scalatra.json.{JValueResult, JacksonJsonSupport}
+import org.scalatra.{NoContent, Ok, ScalatraServlet}
+import org.slf4j.LoggerFactory
 
 case class Topic(text: String, link: String = "") {
   def toJson: JObject = ("text" -> text) ~ ("link" -> link)
 }
 
 class TopicTrackerServlet(val root: String, val chatConfig: JabberConfig) extends ScalatraServlet
-with AtmosphereSupport with SessionSupport
 with JacksonJsonSupport with JValueResult {
   implicit protected val jsonFormats: Formats = DefaultFormats
 
   val logger = LoggerFactory.getLogger(getClass)
   val jabberChat = new JabberChat(chatConfig)
-  var currentTopic = Topic("Default topic", "http://example.org")
-  var currentTopic2: Option[Topic] = None
+  var currentTopic: Option[Topic] = None
 
 
   get("/") {
     "Hello world!"
   }
 
-  atmosphere("/current-topic") {
-    new AtmosphereClient {
-      override def receive: AtmoReceive = {
-        case TextMessage(text) => send(currentTopic.toJson)
-      }
-    }
-  }
-
   get("/topic") {
     contentType = formats("json")
-    currentTopic2 match {
+    currentTopic match {
       case Some(topic) => Ok(topic)
       case None => NoContent()
     }
@@ -51,16 +37,9 @@ with JacksonJsonSupport with JValueResult {
     changeTopic(Topic(values(0), values(1)))
   }
 
-  post("/heartbeat") {
-    AtmosphereClient.broadcast(root + "/current-topic", " ")
-  }
-
   def changeTopic(topic: Topic) {
     logger.info("Changing current topic to: [%s]".format(topic.text))
-
-    currentTopic = topic
-    currentTopic2 = Some(topic)
-    AtmosphereClient.broadcast(root + "/current-topic", currentTopic.toJson)
+    currentTopic = Some(topic)
   }
 
   override def init() {
--- radio-t-server/src/test/scala/org/dandelion/radiot/server/TopicTrackerServletTest.scala
+++ radio-t-server/src/test/scala/org/dandelion/radiot/server/TopicTrackerServletTest.scala
@@ -1,22 +1,14 @@
 package org.dandelion.radiot.server
 
-import java.net.URI
-import org.eclipse.jetty.websocket.client.WebSocketClient
-import scala.concurrent.duration._
-import org.scalatra.test.scalatest.ScalatraSpec
-import org.scalatest.{Matchers, BeforeAndAfter}
-import org.scalatest.concurrent.Eventually
-import org.junit.runner.RunWith
-import org.scalatest.junit.JUnitRunner
-
 import org.json4s.jackson.JsonMethods._
+import org.junit.runner.RunWith
+import org.scalatest.Matchers
+import org.scalatest.junit.JUnitRunner
+import org.scalatra.test.scalatest.ScalatraSpec
 
 
 @RunWith(classOf[JUnitRunner])
-class TopicTrackerServletTest extends ScalatraSpec
-with BeforeAndAfter with Eventually with Matchers {
-  implicit val config = PatienceConfig(timeout = 5 seconds, interval = 0.5 seconds)
-
+class TopicTrackerServletTest extends ScalatraSpec with Matchers {
   val LocalChatConfig = JabberConfig(
     server = "localhost",
     username = "android-radiot",
@@ -25,21 +17,8 @@ with BeforeAndAfter with Eventually with Matchers {
 
   val LocalAdminConfig = LocalChatConfig.copy(username = TopicTrackerServlet.TopicStarter)
 
-  val client = new WebSocketClient
-  val socket = new TopicTrackerSocket
   val servlet = new TopicTrackerServlet("/chat", LocalChatConfig)
-
   addServlet(servlet, servlet.root + "/*")
-
-  before {
-    client.start()
-    client.connect(socket, serverUrl)
-    eventually { socket should be('connected) }
-  }
-
-  after {
-    client.stop()
-  }
 
   it("answers a simple request") {
     get("/chat") {
@@ -67,32 +46,6 @@ with BeforeAndAfter with Eventually with Matchers {
 
   }
 
-
-  ignore("receives current topic after connection is established") {
-    topicShouldBe("Default topic", "http://example.org")
-  }
-
-  it("changes a topic by a message from the chat") {
-    val newTopic = Topic("New topic to discuss", "http://new-topic.org")
-    sendMessageToChat(s"--> ${newTopic.text} ${newTopic.link}")
-    topicShouldBe(newTopic.text, newTopic.link)
-  }
-
-  it("changes a topic by POST request") {
-    val text = "New topic"
-    val link = "http://example.org"
-    val requestBody = text + "\n" + link
-    post("/chat/topic", requestBody) {
-      status should equal(200)
-      topicShouldBe(text, link)
-    }
-  }
-
-  def serverUrl = localPort match {
-    case Some(port) => new URI(s"ws://localhost:$port${servlet.root}/current-topic")
-    case None => throw new RuntimeException("No port is specified")
-  }
-
   def sendMessageToChat(msg: String) {
     new JabberChat(LocalAdminConfig) {
       connect {
@@ -106,9 +59,6 @@ with BeforeAndAfter with Eventually with Matchers {
   def topicJson(newTopic: Topic): String = {
     compact(render(newTopic.toJson))
   }
-
-  def topicShouldBe(text: String, link: String) =
-    eventually { socket.topic should equal(Topic(text, link)) }
 }
 
 
