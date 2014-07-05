e7075197de397c55f616ee8225a40d36c67568a3
Sergey Moshnikov <s.moshnikov@gmail.com>
2014-07-05 08:38:09 +0400

    Refactor for readability in TopicFilterTest

--- radio-t-server/src/test/scala/org/dandelion/radiot/server/TopicFilterTest.scala
+++ radio-t-server/src/test/scala/org/dandelion/radiot/server/TopicFilterTest.scala
@@ -6,26 +6,30 @@ import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class TopicFilterTest extends FunSpec with Matchers {
-  var topic = Topic("", "")
   val TopicId = "topic-id"
   val TopicStarter = "jc-radio-t"
   val TopicStarterJid = "online@conference/" + TopicStarter
+  val InitialTopic = Topic("Initial topic", "http://example.org", "initial-topic-id")
+
+  var topic: Topic = null
   val filter = new TopicFilter(TopicStarter, (newTopic) => topic = newTopic)
 
   it("suppresses messages which are not topic change notifications") {
-    topic = Topic("Current topic")
+    topic = InitialTopic
     filter(TopicId, TopicStarterJid, "Blah-blah from the robot")
-    topic should equal(Topic("Current topic"))
+    topic should equal(InitialTopic)
+  }
+
+  it("suppresses a message if the sender is not a topic starter") {
+    topic = InitialTopic
+    filter(TopicId, "online@conference/some-other-user", "--> New topic started")
+    topic should equal(InitialTopic)
   }
 
   it("extracts the topic to the consumer when a sender is a topic starter") {
+    topic = InitialTopic
     filter(TopicId, TopicStarterJid, "-->   New topic started   http://example.org  ")
     topic should equal(Topic("New topic started", "http://example.org", TopicId))
   }
 
-  it("suppresses a message if the sender is not a topic starter") {
-    topic = Topic("Current topic", "")
-    filter(TopicId, "online@conference/some-other-user", "--> New topic started")
-    topic should equal(Topic("Current topic"))
-  }
 }
