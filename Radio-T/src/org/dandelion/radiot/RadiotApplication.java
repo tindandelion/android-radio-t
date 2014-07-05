fb574fcc14d7967ab4b8c1e37ddda101666967b2
Sergey Moshnikov <s.moshnikov@gmail.com>
2014-07-05 11:28:08 +0400

    Switched to production

--- Radio-T/src/org/dandelion/radiot/RadiotApplication.java
+++ Radio-T/src/org/dandelion/radiot/RadiotApplication.java
@@ -2,13 +2,12 @@ package org.dandelion.radiot;
 
 import android.app.Application;
 import android.os.Handler;
-import android.util.Log;
 import org.dandelion.radiot.common.Scheduler;
 import org.dandelion.radiot.http.DataEngine;
 import org.dandelion.radiot.http.HttpDataEngine;
 import org.dandelion.radiot.http.Provider;
 import org.dandelion.radiot.live.chat.HttpChatClient;
-import org.dandelion.radiot.live.topics.CurrentTopic;
+import org.dandelion.radiot.live.topics.HttpTopicProvider;
 import org.dandelion.radiot.live.ui.ChatTranslationFragment;
 import org.dandelion.radiot.live.ui.CurrentTopicFragment;
 import org.dandelion.radiot.podcasts.main.PodcastsApp;
@@ -17,7 +16,8 @@ import org.dandelion.radiot.util.ProgrammerError;
 public class RadiotApplication extends Application {
     private static final String CHAT_URL = "http://chat.radio-t.com";
     private static final String TOPIC_TRACKER_BASE_URL = "http://107.170.84.215:8080";
-    // private static final String CHAT_URL = "http://192.168.5.206:4567";
+    //private static final String TOPIC_TRACKER_BASE_URL = "http://10.0.1.2:8080";
+    // private static final String CHAT_URL = "http://10.0.1.2:4567";
 
     public static DataEngine createChatTranslation(String chatUrl, Scheduler scheduler) {
         return new HttpDataEngine<>(HttpChatClient.create(chatUrl), scheduler);
@@ -36,19 +36,19 @@ public class RadiotApplication extends Application {
         CurrentTopicFragment.trackerFactory = new DataEngine.Factory() {
             @Override
             public DataEngine create() {
-                // Provider provider = new HttpTopicProvider(TOPIC_TRACKER_BASE_URL);
-                Provider provider = new Provider<CurrentTopic>() {
-                    @Override
-                    public CurrentTopic get() throws Exception {
-                        Log.d("TOPIC", "Requested new topic");
-                        Thread.sleep(2000);
-                        return new CurrentTopic("topic",
-                                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquet, massa ut tincidunt pellentesque, dui nibh lacinia magna, elementum ultrices magna risus a tellus");
-                    }
-
-                    @Override
-                    public void abort() { }
-                };
+                 Provider provider = new HttpTopicProvider(TOPIC_TRACKER_BASE_URL);
+//                Provider provider = new Provider<CurrentTopic>() {
+//                    @Override
+//                    public CurrentTopic get() throws Exception {
+//                        Log.d("TOPIC", "Requested new topic");
+//                        Thread.sleep(2000);
+//                        return new CurrentTopic("topic",
+//                                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquet, massa ut tincidunt pellentesque, dui nibh lacinia magna, elementum ultrices magna risus a tellus");
+//                    }
+//
+//                    @Override
+//                    public void abort() { }
+//                };
                 return new HttpDataEngine(provider, new HandlerScheduler(updateDelayMillis));
             }
         };
