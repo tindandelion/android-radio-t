fd21d69e7943032114f7450d0c6840f2bd948c9e
Sergey Moshnikov <s.moshnikov@gmail.com>
2015-03-04 07:10:03 +0200

    Fixed acceptance tests failures on Android 5.0 caused by longer timeout for playback errors.

--- Radio-T-Test/src/org/dandelion/radiot/endtoend/live/helpers/LiveShowUiDriver.java
+++ Radio-T-Test/src/org/dandelion/radiot/endtoend/live/helpers/LiveShowUiDriver.java
@@ -7,6 +7,14 @@ import com.robotium.solo.Solo;
 import static junit.framework.Assert.assertTrue;
 
 public class LiveShowUiDriver extends Solo {
+
+    // 60 seconds to wait for the stupid MediaPlayer to detect that the
+    // audio stream is not playable (HTTP error 404). Prior to Android 5.0
+    // MediaPlayer.prepareAsync() returned instantly, in Android 5.0 it
+    // makes several attempts to connect to the stream, so it only returns after
+    // 40 seconds or so.
+    public static final int TRANSLATION_TIMEOUT = 60_000;
+
     public LiveShowUiDriver(Instrumentation instrumentation, Activity activity) {
         super(instrumentation, activity);
     }
@@ -16,7 +24,7 @@ public class LiveShowUiDriver extends Solo {
     }
 
     public void showsTranslationStatus(String statusText) {
-        assertTrue(waitForText(statusText));
+        assertTrue(waitForText(statusText, 0, TRANSLATION_TIMEOUT));
     }
 
 }
