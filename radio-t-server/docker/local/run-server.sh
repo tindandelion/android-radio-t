ccfca33cc84c117179540aca1761408e6e2aa325
Sergey Moshnikov <s.moshnikov@gmail.com>
2015-05-02 14:39:14 +0300

    Shell script to start the container

---
+++ radio-t-server/docker/local/run-server.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+docker run -d \
+  -v /vagrant/docker/local/etc/radio-t-server.conf:/etc/radio-t-server.conf \
+  --link xmpp-server:xmpp-server \
+  -p 8080:80 \
+  sergeymo/radio-t-server:1.0
