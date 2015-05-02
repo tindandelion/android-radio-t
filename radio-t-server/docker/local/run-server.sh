9e20e008f03b5b427a18f99208ef59015da18352
Sergey Moshnikov <s.moshnikov@gmail.com>
2015-05-02 17:42:56 +0300

    Added a task to start Docker container

--- radio-t-server/docker/local/run-server.sh
+++ radio-t-server/docker/local/run-server.sh
@@ -3,4 +3,5 @@ docker run -d \
   -v /vagrant/docker/local/etc/radio-t-server.conf:/etc/radio-t-server.conf \
   --link xmpp-server:xmpp-server \
   -p 8080:80 \
+  --name radio-t-server \
   sergeymo/radio-t-server:1.0
