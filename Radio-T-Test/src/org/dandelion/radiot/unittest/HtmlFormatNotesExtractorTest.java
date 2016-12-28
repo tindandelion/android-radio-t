e76b3683fb6de4437bb8422bbd6b288d9af33d23
Sergey Moshnikov <s.moshnikov@gmail.com>
2016-12-28 15:31:46 +0200

    HtmlFormatNotesExtractor produces a nice text

--- Radio-T-Test/src/org/dandelion/radiot/unittest/HtmlFormatNotesExtractorTest.java
+++ Radio-T-Test/src/org/dandelion/radiot/unittest/HtmlFormatNotesExtractorTest.java
@@ -4,18 +4,36 @@ import junit.framework.TestCase;
 import org.dandelion.radiot.podcasts.loader.rss.HtmlFormatNotesExtractor;
 
 public class HtmlFormatNotesExtractorTest extends TestCase {
+    private final HtmlFormatNotesExtractor extractor = new HtmlFormatNotesExtractor();
 
     public void test_extractSimpleText_makesNoChanges() throws Exception {
-        HtmlFormatNotesExtractor extractor = new HtmlFormatNotesExtractor();
         String text = "very simple text";
-
         assertEquals(text, extractor.extract(text));
     }
 
-    public void test_extractFromHtml_omitsHtmlTags() throws Exception {
-        HtmlFormatNotesExtractor extractor = new HtmlFormatNotesExtractor();
-        String text = "<p>html-formatted <a href=\"http://google.fi\">text</a>";
+    public void test_removeLeadingAndTrainingBlanks() throws Exception {
+        String text = "   text with blanks   ";
+        assertEquals("text with blanks", extractor.extract(text));
+    }
 
+    public void test_extractFromHtml_omitsHtmlFormattingTags() throws Exception {
+        String text = "<p>html-formatted <a href=\"http://google.fi\">text</a>";
         assertEquals("html-formatted text", extractor.extract(text));
+    }
+
+
+    public void test_extractFromHtml_withImages() throws Exception {
+        String text = "<img src=\"https://radio-t.com/images/radio-t/rt526.jpg\" alt=\"\">text";
+        assertEquals("text", extractor.extract(text));
+    }
+
+    public void test_extractFromHtml_listItems() throws Exception {
+        String text = "<li>item1</li><li>item2</li>";
+        assertEquals("● item1 ● item2", extractor.extract(text));
+    }
+
+    public void test_replaceNewLines_withTwoSpaces() throws Exception {
+        String text = "<ul><li>line1</li></ul><p>line2</p>";
+        assertEquals("● line1  line2", extractor.extract(text));
     }
 }
--- Radio-T/src/org/dandelion/radiot/podcasts/loader/rss/HtmlFormatNotesExtractor.java
+++ Radio-T/src/org/dandelion/radiot/podcasts/loader/rss/HtmlFormatNotesExtractor.java
@@ -1,14 +1,37 @@
 package org.dandelion.radiot.podcasts.loader.rss;
 
+import android.text.Editable;
 import android.text.Html;
+import android.text.Spanned;
+import org.xml.sax.XMLReader;
 
 public class HtmlFormatNotesExtractor implements RssParser.NotesExtractor {
+
+    private static final Html.TagHandler tagHandler = new Html.TagHandler() {
+        @Override
+        public void handleTag(boolean opening, String tag, Editable output, XMLReader xmlReader) {
+            if (opening && "li".equals(tag)) {
+                output.append(" ● ");
+            }
+        }
+    };
+
     @Override
     public String extract(String text) {
-        return trimBlanks(Html.fromHtml(text).toString());
+        Spanned spanned = Html.fromHtml(text, null, tagHandler);
+        return trimBlanks(removeLineBreaks(removeImageMarkers(spanned.toString())));
+    }
+
+    private String removeLineBreaks(String s) {
+        return s.replaceAll("\n", " ");
+    }
+
+    private String removeImageMarkers(String s) {
+        final String imgMarker = "\uFFFC";
+        return s.replaceAll(imgMarker, "");
     }
 
     private String trimBlanks(String s) {
-        return s.replaceAll("\\s*$", "");
+        return s.replaceAll("\\s*$|^\\s*", "");
     }
 }
