f0cf175e652d0692fbeb0b380a433747d8f26f0a
Sergey Moshnikov <s.moshnikov@gmail.com>
2017-01-05 09:27:55 +0200

    Removed radio-t-server, deprecated

--- radio-t-server/docker/local/run-server.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-docker run -d \
-  -v /vagrant/docker/local/etc/radio-t-server.conf:/etc/radio-t-server.conf \
-  --link xmpp-server:xmpp-server \
-  -p 8080:80 \
-  --name radio-t-server \
-  sergeymo/radio-t-server:1.0
--- radio-t-server/src/main/scala/org/dandelion/radiot/server/JabberConfig.scala
+++
@@ -1,5 +0,0 @@
-package org.dandelion.radiot.server
-
-case class JabberConfig(server: String, username: String, password: String, room: String) {
-  val port = 5222
-}
