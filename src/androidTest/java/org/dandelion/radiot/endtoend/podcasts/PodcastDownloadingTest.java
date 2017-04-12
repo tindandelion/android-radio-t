d26ac76a2cd56db9627d618a3722d0c8bc8adc66
Sergey Moshnikov <s.moshnikov@gmail.com>
2017-04-12 06:10:45 +0300

    Working on a test for downloading podcasts

--- src/androidTest/java/org/dandelion/radiot/endtoend/podcasts/PodcastDownloadingTest.java
+++ src/androidTest/java/org/dandelion/radiot/endtoend/podcasts/PodcastDownloadingTest.java
@@ -2,14 +2,21 @@ package org.dandelion.radiot.endtoend.podcasts;
 
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import org.dandelion.radiot.podcasts.core.PodcastItem;
+import org.dandelion.radiot.podcasts.core.PodcastList;
 import org.dandelion.radiot.podcasts.ui.PodcastListActivity;
 import org.dandelion.radiot.podcasts.ui.PodcastListModel;
+import org.dandelion.radiot.podcasts.ui.PodcastVisual;
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.fail;
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.instanceOf;
 
 @RunWith(AndroidJUnit4.class)
 public class PodcastDownloadingTest {
@@ -25,15 +32,54 @@ public class PodcastDownloadingTest {
 
     @Test
     @Ignore
-    public void first() throws Exception {
-        fail("This test runs fine");
+    public void downloadPodcast() throws Exception {
+        onData(instanceOf(PodcastVisual.class))
+                .atPosition(0)
+                .perform(click());
+
+        onView(withText("Загрузить"))
+                .perform(click());
+
     }
 
-    private class TestPodcastPlatform implements PodcastListModel.Factory {
+    private class TestPodcastPlatform implements PodcastListModel.Factory, PodcastListModel {
+
+        private Consumer consumer;
 
         @Override
         public PodcastListModel create(String showName) {
-            return null;
+            return this;
+        }
+
+        @Override
+        public void attach(ProgressListener progressListener, Consumer consumer) {
+            this.consumer = consumer;
+        }
+
+        @Override
+        public void release() {
+
+        }
+
+        @Override
+        public void populateConsumer() {
+            consumer.updateList(sampleList());
+        }
+
+        private PodcastList sampleList() {
+            PodcastItem item = new PodcastItem();
+            item.title = "111";
+            item.showNotes = "Test notes";
+            item.audioUri = "http://cdn.radio-t.com/rt_podcast540.mp3";
+
+            PodcastList list = new PodcastList();
+            list.add(item);
+            return list;
+        }
+
+        @Override
+        public void refreshData() {
+
         }
     }
 }
